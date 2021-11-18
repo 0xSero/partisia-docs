d6efab614dde54f52eebbd688f4bb2c2253ff051
Jesenko Mehmedbasic <jm@secata.com>
2021-11-18 21:28:05 +0100

    Nicer latex, more docs

--- documentation/docs/js/mathjax-conf.js
+++ documentation/docs/js/mathjax-conf.js
@@ -1,16 +1,36 @@
-window.MathJax = {
-    tex: {
-        inlineMath: [["\\(", "\\)"]],
-        displayMath: [["\\[", "\\]"]],
-        processEscapes: true,
-        processEnvironments: true
-    },
-    options: {
-        ignoreHtmlClass: ".*|",
-        processHtmlClass: "arithmatex"
-    }
-};
 
-document$.subscribe(() => {
-    MathJax.typesetPromise()
-})
+var katexMath = (function () {
+    var maths = document.querySelectorAll('.arithmatex'),
+        tex;
+
+    for (var i = 0; i < maths.length; i++) {
+        tex = maths[i].textContent || maths[i].innerText;
+        if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+            katex.render(tex.slice(2, -2), maths[i], {'displayMode': false});
+        } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+            katex.render(tex.slice(2, -2), maths[i], {'displayMode': true});
+        }
+    }
+});
+
+(function () {
+    var onReady = function onReady(fn) {
+        if (document.addEventListener) {
+            document.addEventListener("DOMContentLoaded", fn);
+        } else {
+            document.attachEvent("onreadystatechange", function () {
+                if (document.readyState === "interactive") {
+                    fn();
+                }
+            });
+        }
+    };
+
+    onReady(function () {
+        if (typeof katex !== "undefined") {
+            katexMath();
+        }
+    });
+})();
+
+}());
